22564ddc6eaca02a38d9ee863007cb07579fbaea
Mizuyusan <88479555+shinkungoo@users.noreply.github.com>
2024-07-31 00:02:58 +0800

    Build File Updating

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from setuptools import setup, find_packages
 
-test_deps = [
-    'pytest>=4',
-    'pytest-cov>=2.6.0',
-    'pytest-flake8<5.0.0',
-    'flake8<5.0.0'
-]
-
 setup(
     name='inscd',
     version='1.1.0',
-    extras_require={
-        'test': test_deps,
-    },
+    author='Junhao Shen',
+    author_email='shenjh@stu.ecnu.edu.cn',
+    license='MIT license',
     packages=find_packages(),
     install_requires=[
         "torch", "tqdm", "numpy>=1.16.5", "scikit-learn", "pandas", "dgl"
